2d273651b18c8bb8dd4329b949f4385fcadd5157
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2023-01-22 06:35:34 +0000

    Weather Project Update

--- src/Cards.js
+++ src/Cards.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import WeatherImage from './cloudy.png'
+import WeatherImage from './unknown.png'
+import loading from './loading.png';
+
 
 function Cards() {
     return (
         <div className='AllCards'>
-            <NewCard />
-            <NewCard />
-            <NewCard />
-            <NewCard />
-            <NewCard />
+            <LoadingSVG />
+            <NewCard1 />
+            <NewCard2 />
+            <NewCard3 />
+            <NewCard4 />
+            <NewCard5 />
         </div>
     )
 }
 
-function NewCard() {
+function LoadingSVG() {
     return (
-        <div className='newCard'>
-            <div className='weekDay'>Friday</div>
-            <div className='weekDate'>March 06th, 1:00pm</div>
-            <img className='weatherImage' src={WeatherImage} alt='image'></img>
-            <div className='temp'>26゜C</div>
-            <div className='condition'>Cloudy</div>
+        <img src={loading} id="loading" className="loadingImg linear infinite" alt='loading...' />
+    )
+}
+
+function NewCard1() {
+    return (
+        <div className='newCard newCard0' id="showCards0">
+            <div className='weekDay' id="weekDay0">Friday</div>
+            <div className='weekDate' id="weekDate0">March 06th, 1:00pm</div>
+            <img className='weatherImage' id="weatherImg0" src={WeatherImage} alt='image'></img>
+            <div className='temp' id="temp0">26゜C</div>
+            <div className='condition' id="condition0">Cloudy</div>
+        </div>
+    )
+}
+
+function NewCard2() {
+    return (
+        <div className='newCard' id="showCards1">
+            <div className='weekDay' id="weekDay1">Friday</div>
+            <div className='weekDate' id="weekDate1">March 06, 01:00</div>
+            <img className='weatherImage' id="weatherImg1" src={WeatherImage} alt='image'></img>
+            <div className='temp' id="temp1">26゜C</div>
+            <div className='condition' id="condition1">Cloudy</div>
+        </div>
+    )
+}
+function NewCard3() {
+    return (
+        <div className='newCard' id="showCards2">
+            <div className='weekDay' id="weekDay2">Friday</div>
+            <div className='weekDate' id="weekDate2">March 06th, 1:00pm</div>
+            <img className='weatherImage' id="weatherImg2" src={WeatherImage} alt='image'></img>
+            <div className='temp' id="temp2">26゜C</div>
+            <div className='condition' id="condition2">Cloudy</div>
+        </div>
+    )
+}
+function NewCard4() {
+    return (
+        <div className='newCard' id="showCards3">
+            <div className='weekDay' id="weekDay3">Friday</div>
+            <div className='weekDate' id="weekDate3">March 06th, 1:00pm</div>
+            <img className='weatherImage' id="weatherImg3" src={WeatherImage} alt='image'></img>
+            <div className='temp'  id="temp3">26゜C</div>
+            <div className='condition' id="condition3">Cloudy</div>
+        </div>
+    )
+}
+function NewCard5() {
+    return (
+        <div className='newCard' id="showCards4">
+            <div className='weekDay' id="weekDay4">Friday</div>
+            <div className='weekDate' id="weekDate4">March 06th, 1:00pm</div>
+            <img className='weatherImage' id="weatherImg4" src={WeatherImage} alt='image'></img>
+            <div className='temp' id="temp4">26゜C</div>
+            <div className='condition' id="condition4">Cloudy</div>
         </div>
     )
 }
--- src/CurrentLocation.js
+++ src/CurrentLocation.js
@@ -1,9 +1,18 @@
+/* eslint-disable default-case */
 import React from "react";
 import axios from "axios";
+import Clear from './clear.png';
+import Rainy from './rainy.png';
+import Cloudy from './cloudy.png';
 
 let lat;
 let long;
 let city;
+let weather;
+let date;
+let e;
+
+
 
 
 function getLocation() {
@@ -19,6 +28,139 @@ function mapCoords(position) {
     long =  position.coords.longitude;
     console.log(lat + " " + long);
     getCountry(lat, long);
+    getWeather(lat, long);
+}
+
+function getWeather(lat, long) {
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=ae8ef316f929e83a1e9a01e829e33d89";
+    axios.get(weatherUrl)
+        .then(response => {
+            weather = response.data;
+            document.getElementById("loading").style.opacity = 0;
+            for (let t=0; t<5; t++ ){
+                // eslint-disable-next-line default-case
+                switch (t) {
+                    case 0:
+                        e = 2;
+                        break
+                    case 1:
+                        e = 8;
+                        break
+                    case 2:
+                        e = 14;
+                        break
+                    case 3:
+                        e = 24
+                        break
+                    case 4:
+                        e = 32;
+                        break
+                }
+                    
+                let currtemp = weather.list[e].main.temp;
+                currtemp = Math.floor(currtemp - 273);
+                document.getElementById("temp" + t).innerText = currtemp + "゜C";
+                let currWeather = weather.list[e].weather[0].main;
+                if (currWeather === "Clear") {
+                    document.getElementById("weatherImg" + t).src = Clear;
+                } else if (currWeather === "Rainy") {
+                    document.getElementById("weatherImg" + t).src = Rainy;
+                } else if (currWeather === "Clouds") {
+                    document.getElementById("weatherImg" + t).src = Cloudy;
+                }
+
+                document.getElementById("condition" + t).innerText = currWeather;
+                date = new Date(weather.list[e].dt_txt);
+                let day = date.getDay();
+                let tempDate = date.getDate();
+                let month = date.getMonth();
+                let hours = date.getHours();
+                let min = date.getMinutes();
+
+                // eslint-disable-next-line default-case
+                switch (day) {
+                    case 1:
+                        day = "Monday";
+                        break;
+                    case 2:
+                        day = "Tuesday";
+                        break;
+                    case 3:
+                        day = "Wednesday";
+                        break
+                    case 4:
+                        day = "Thrusday";
+                        break;
+                    case 5:
+                        day = "Friday";
+                        break;
+                    case 6:
+                        day = "Saturday";
+                        break;
+                    case 0:
+                        day = "Sunday";
+                        break;
+                }
+
+                document.getElementById("weekDay" + t).innerText = day;
+
+                switch (month) {
+                    case 0:
+                        month = "January";
+                        break;
+                    case 1:
+                        month = "February";
+                        break;
+                    case 2:
+                        month = "March";
+                        break;
+                    case 3:
+                        month = "April";
+                        break;
+                    case 4:
+                        month = "May";
+                        break;
+                    case 5:
+                        month = "June";
+                        break;
+                    case 6:
+                        month = "July";
+                        break;
+                    case 7:
+                        month = "August";
+                        break;
+                    case 8:
+                        month = "September";
+                        break; 
+                    case 9:
+                        month = "October";
+                        break;
+                    case 10:
+                        month = "November";
+                        break;
+                    case 11:
+                        month = "December";
+                        break;
+                }
+
+                if (hours<10) {
+                    hours = "0" + hours;
+                }
+
+                let createDate = month + " " + tempDate + ", " + hours + ":" + min + "0";
+                document.getElementById("weekDate" + t).innerText = createDate;
+                // document.getElementById("weekDay" + t).innerText = day;
+
+
+                document.getElementById("showCards" + t).style.opacity = 1;
+            }
+            
+            
+        })
+        .catch(error => {
+            weather = error;
+            return(error);
+        });
 }
 
 function getCountry(lat, long) {
@@ -40,6 +182,7 @@ function CurrentLoc() {
     getLocation()
     return (
         <div className="location" id="location">...</div>
+        
     )
 }
 
